a6ef3dfc735b7e885cad3b4e089025d88c364c64
abdulrehman486 <175531756+abdulrehman486@users.noreply.github.com>
2024-07-16 08:34:37 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code in new version
+ //alert(3+3)//we are using node js not browser 
+ 
+console.log(3+3)
+//code readability sould be high
+console.log("abdul")
+let name="abdul"
+let age= 22
+let isloggedIn=false
+
+// number=> 2 to power 53
+// bigint
+//string=>""
+//boolean=>true/fals
+// null=> standalone value
+//undefined
+//symbols=>unique
+
+// object
+  console.log(typeof"abdul")
+console.log(typeof undefined);//undefined
+console.log(typeof null);//null
