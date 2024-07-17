92e16bb1e6967827e1f6f1e77c1a4bb95525c821
abdulrehman486 <175531756+abdulrehman486@users.noreply.github.com>
2024-07-17 08:58:17 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversion operation.js
@@ -0,0 +1,17 @@
+let score = "abdul"
+  
+console.log(typeof (score) )
+
+let valueInnumber = Number(score)
+//console.log (typeof valueInnumber)
+//console.log(valueInnumber)
+ //"33"=>33
+ // "33abc"=>NaN
+ // true=>1;false=> 0
+  let isloggedIn = 1
+  let booleanisloggedIn=Boolean(isloggedIn)
+   console.log(booleanisloggedIn)
+
+   //true=>1; false=>0
+   //""=>false
+   //"tabish"=>true
