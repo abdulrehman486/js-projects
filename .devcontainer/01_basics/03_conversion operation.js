be76516acc9846f95cae41e076d4ad0cd7043389
abdulrehman486 <175531756+abdulrehman486@users.noreply.github.com>
2024-07-18 08:46:44 +0000

    operation

--- .devcontainer/01_basics/03_conversion operation.js
+++ .devcontainer/01_basics/03_conversion operation.js
@@ -15,3 +15,23 @@ let valueInnumber = Number(score)
    //true=>1; false=>0
    //""=>false
    //"tabish"=>true
+//            operation//
+
+   //   operations
+   let value = 3
+   let negvalue = -value
+    console.log(negvalue)
+ 
+   //  console.log(2+2)
+    // console.log(2-2)
+     //console.log(2*2)
+     //console.log(2/2)
+     //console.log(2**2)
+     //console.log(2%2)
+       
+     let str1 = "hello"
+      let str2= " Abdulrehman"
+       let  str3=  "str1+str2"
+       console.log(str1+str2)
+ 
+      
